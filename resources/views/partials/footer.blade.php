1861df8e5b83ef0e53a9abe2858cd625565ede4b
GiovanniCadoni <giocadoni19@gmail.com>
2024-01-15 22:24:31 +0100

    add: header and footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,65 @@
+<footer>
+    <div class="container-top">
+        <div class="content">
+            <ul class="components">
+                <li class="list">
+                    <ul>
+                        <li class="title">DC COMICS</li>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                    <ul>
+                        <li class="title">SHOP</li>
+                        <li>Shop DC</li>
+                        <li>Shop DC collectibles</li>
+                    </ul>
+                </li>
+                <li class="list">
+                    <ul>
+                        <li class="title">DC</li>
+                        <li>Terms of us</li>
+                        <li>Privacy Policy</li>
+                        <li>Ad choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subscription</li>
+                        <li>Talent Workshop</li>
+                        <li>CPSC certificates</li>
+                        <li>Ratings</li>
+                        <li>Shop help</li>
+                        <li>Contact us</li>
+                    </ul>
+                </li>
+                <li class="list">
+                    <ul>
+                        <li class="title">SITES</li>
+                        <li>DC</li>
+                        <li>MAD magazine</li>
+                        <li>DC kids</li>
+                        <li>DC universe</li>
+                        <li>DC power visa</li>
+                    </ul>
+                </li>
+                
+            </ul>
+        </div>
+    </div>
+    <div class="container-bottom">
+        <div class="content">
+            <button>SIGN-UP NOW!</button>
+            <ul>
+                <li>FOLLOW US</li>
+                <li><img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt=""></li>
+                <li><img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt=""></li>
+                <li><img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt=""></li>
+                <li><img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt=""></li>
+                <li><img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt=""></li>
+            </ul>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,42 @@
+<header>
+    <div class="container">
+        <div class="header">
+            <img class="logo" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+            <ul>
+                <li>
+                    <a href="">CHARACTERS</a>
+                </li>
+                <li>
+                    <a href="">COMICS</a>
+                </li>
+                <li>
+                    <a href="">MOVIES</a>
+                </li>
+                <li>
+                    <a href="">TV</a>
+                </li>
+                <li>
+                    <a href="">GAMES</a>
+                </li>
+                <li>
+                    <a href="">COLLECTIBLES</a>
+                </li>
+                <li>
+                    <a href="">VIDEOS</a>
+                </li>
+                <li>
+                    <a href="">FANS</a>
+                </li>
+                <li>
+                    <a href="">NEWS</a>
+                </li>
+                <li>
+                    <a href="">SHOP</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="jumbotron">
+        <button>CURRENT SERIES</button>
+    </div>
+</header>
