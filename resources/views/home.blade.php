37daca3e05164bc15d1a2615e9b134bfa4e3bad6
GiovanniCadoni <giocadoni19@gmail.com>
2024-01-16 18:43:28 +0100

    add: comics database and its card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-    
+    <main class="container">
+        <div class="row row-cols-4">
+            @foreach ($comics as $comic)
+                <div class="col h-100">
+                    <img class="" src="{{ $comic['thumb'] }}" alt="">
+                    <h3 class="text-white">
+                        {{ $comic['series'] }}
+                    </h3>
+                </div>
+            @endforeach
+        </div>
+    </main>
 @endsection
